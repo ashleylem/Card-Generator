51e3b7cee377981bf8d30d332c167d47da0fb74d
ashleylem <ashley.lemus@yahoo.com>
2022-11-03 14:56:06 +0000

    project done

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let suite = ["♥", "♠", "♣", "♦"];
+let cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+let cardIndex = [Math.floor(Math.random() * cards.length)];
+let suiteIndex = [Math.floor(Math.random() * suite.length)];
+
+let element = document.querySelector(".card");
+
+document.querySelector(".content").innerHTML = cards[cardIndex];
+document.querySelector(".header").innerHTML = suite[suiteIndex];
+document.querySelector(".footer").innerHTML = suite[suiteIndex];
+
+if (suite[suiteIndex] === "♥" || suite[suiteIndex] === "♦") {
+  element.style.color = "red";
+}
